cd7270e00305281a179d0a38c2064bceafd8e576
Nathaniel Nguyen <nonguyen@ucsd.edu>
2018-11-12 14:49:50 -0800

    Experiment with TF

--- src/models/VanillaRNN_tf.py
+++ src/models/VanillaRNN_tf.py
@@ -1,6 +1,4 @@
 # Vanilla RNN used for semantic analysis build with low-level tensorflow.
-# Note: Should compate with the keras RNN to test performance of
-#   implementation.
 
 import tensorflow as tf
 import numpy as np
@@ -11,44 +9,31 @@ class RNN_Model(object):
     Description:
     Function:
         __init__() -- Build the RNN object instantiate all basic parameters.
-        train() -- Train the model
-        predict()
-        score()
-    '''
+            Loads any parameters that might be saved. 
+        predict() -- Generate labels based on input.
+        score() -- Generate labels and determine the accuaracy of the model
+            based on the inputs '''
 
     def __init__(self, num_layers=10, training=True):
-        ''' Constructor '''
-        # Is this model used for training. If so, then we need to update the
-        # weights everytime we pass in inputs.
-        self.training = training; 
-        self.input_features = tf.placeholder(tf.float32, [None,])
-        self.input_labels = tf.placeholder(tf.int32, [None,])
-        cells = []
-        for _ in range(num_layers):
-            cell = rnn.RNNCell
-            cell = rnn.DropoutWrapper(cell, input_keep_prob=True,
-                output_keep_prob=True)
-            cells.append(cell)
-        self.cell = cell = rnn.MultiRNNCell(cells, state_is_tuple=True)
+        '''
+        Build the main functionality of the model
+
+        Notes: Delete this
+          1) Need a way to load in past parameters to allow for more training
+          if needed
+          2) Build the main data input stream into the model.
+          3) Build the each node.
+          4) Define the execution graph that will be ran when called on.
+        '''
+        pass
 
     def train(lr=0.001):
-        '''
-        Train the model with 
-        Params:
-            lr -- learning rate for the optimization function.
-        '''
         pass
 
     def predict():
-        '''
-        Use trained model to make predictions on unseen data
-        '''
         pass
 
     def score():
-        '''
-        Get the accuracy of the sample
-        '''
         pass
 
 if __name__ == "__main__":
@@ -56,3 +41,5 @@ if __name__ == "__main__":
     test_model = RNN_Model()
     print("Good to go")
 
+# Note: Should compate with the keras RNN to test performance of
+#   implementation.
